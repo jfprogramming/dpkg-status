679238b23de265b9706286b236c502c3fa54f7c7
jesse finneman <jfskibum123@yahoo.com>
2025-04-14 03:47:19 -0600

    clean up adding in qt app

--- qt/main.cpp
+++ qt/main.cpp
@@ -13,27 +13,12 @@ int main(int argc, char *argv[])
 
     QQmlApplicationEngine engine;
 
-    // Connect to the warnings signal to log QML errors
-    QObject::connect(&engine, &QQmlApplicationEngine::warnings, [](const QList<QQmlError> &warnings) {
-        for (const auto &warning : warnings) {
-            qCritical() << "QML Warning:" << warning.toString();
-        }
-    });
-
     // Register the DpkgModelData class with QML
     DpkgModelData tableModel;
     engine.rootContext()->setContextProperty("tableModel", &tableModel);
 
-    qDebug() << "Resource Test:" << QUrl(QStringLiteral("qrc:/Main.qml")).isValid();
-    qDebug() << "Resource Exists:" << QFile::exists(":/qml/Main.qml");
     // Load QML file from the file system
     engine.load(QUrl(QStringLiteral("qrc:/qt/qml/dpkgstatus/qml/Main.qml")));
-
-    QDirIterator it(":", QDirIterator::Subdirectories);
-    while (it.hasNext()) {
-        qDebug() << "Available Resource:" << it.next();
-    }
-
     if (engine.rootObjects().isEmpty()) {
         qCritical() << "Failed to load QML file.";
         return -1;
