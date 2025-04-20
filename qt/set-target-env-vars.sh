1e44c57507f79ee29fc0ddae2766cf554647e8de
jesse finneman <jfskibum123@yahoo.com>
2025-04-19 23:46:58 -0600

    v1.0.0 clean up

--- qt/set-target-env-vars.sh
+++ qt/set-target-env-vars.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 
-# Set the application installation directory
-APP_INSTALL_DIR="/usr/lib/appdpkg-status"
-
 # Set target environment variables
 export QML2_IMPORT_PATH="/usr/share/appdpkg-status/qml/"
-export LD_LIBRARY_PATH="/usr/lib/appdpkg-status/"
-export QT_PLUGIN_PATH="/usr/lib/appdpkg-status/"
-export QT_QUICK_BACKEND=software
+export LD_LIBRARY_PATH="/usr/lib/appdpkg-status/:$LD_LIBRARY_PATH"
+export QT_PLUGIN_PATH="/usr/lib/appdpkg-status/:$QT_PLUGIN_PATH"
+export QT_QUICK_BACKEND="software"
 
 # Print environment variables for verification
 echo "Environment variables set for this session:"
@@ -20,10 +17,9 @@ echo "QT_PLUGIN_PATH=$QT_PLUGIN_PATH"
 read -p "Do you want to make these changes permanent? (y/n): " choice
 if [[ "$choice" == "y" || "$choice" == "Y" ]]; then
     if [[ -w ~/.bashrc ]]; then
-        echo "export PATH=\"$APP_INSTALL_DIR/bin:\$PATH\"" >> ~/.bashrc
-        echo "export LD_LIBRARY_PATH=\"$APP_INSTALL_DIR:\$APP_INSTALL_DIR/platforms:\$LD_LIBRARY_PATH\"" >> ~/.bashrc
-        echo "export QML2_IMPORT_PATH=\"$APP_INSTALL_DIR/qml:\$QML2_IMPORT_PATH\"" >> ~/.bashrc
-        echo "export QT_PLUGIN_PATH=\"$APP_INSTALL_DIR/platforms:\$QT_PLUGIN_PATH\"" >> ~/.bashrc
+        echo "export LD_LIBRARY_PATH=\"/usr/lib/appdpkg-status:\$LD_LIBRARY_PATH\"" >> ~/.bashrc
+        echo "export QML2_IMPORT_PATH=\"/usr/share/appdpkg-status/qml:\$QML2_IMPORT_PATH\"" >> ~/.bashrc
+        echo "export QT_PLUGIN_PATH=\"/usr/lib/appdpkg-status:\$QT_PLUGIN_PATH\"" >> ~/.bashrc
         echo "export QT_QUICK_BACKEND=\"software\"" >> ~/.bashrc
         echo "Environment variables have been added to ~/.bashrc."
         echo "Please restart your terminal or run 'source ~/.bashrc' to apply the changes."
